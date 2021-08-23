1abf595ca809264fc3cd2fd05ee6621832f08135
Oleg Ivanov <gmiolegv@gmail.com>
2021-08-23 05:14:32 +0300

    new app

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import { AppLoading } from "expo";
+import { bootstrap } from "./src/bootstrap";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  if (isReady) {
+    return (
+      <AppLoading
+        startAsinc={bootstrap}
+        onFinish={() => setIsReady(false)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -10,12 +24,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
